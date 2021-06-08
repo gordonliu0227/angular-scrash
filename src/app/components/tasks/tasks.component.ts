b7c98b634ff9ca9416a2222f46aa07deffcedc00
Gordon <gordonliu0227@users.noreply.github.com>
2021-06-08 17:38:55 +1000

    first commit

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,20 @@
+import { Task } from './../../Task';
+import { TASKS } from './../../mock-task';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = TASKS;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
